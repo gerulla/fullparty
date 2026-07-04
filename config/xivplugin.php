689734633fe32fcc620db94ac9f040ab62d864b9
G3ru1a <gerula2k25@gmail.com>
2026-07-04 22:46:15 +0100

    added readycheck and countdown commands for plugin socket

--- config/xivplugin.php
+++ config/xivplugin.php
@@ -6,6 +6,8 @@ return [
             'place_markers',
             'clear_markers',
             'sync_timer',
+            'ready_check',
+            'countdown',
             'open_overlay',
             'show_message',
         ],
